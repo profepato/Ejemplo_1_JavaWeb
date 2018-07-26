09e4eae6b2a25587618a02c4b696156da7569e03
pperezp <patodeath@gmail.com>
2018-07-25 23:45:02 -0400

    Finalizando el ejemplo

--- src/java/controller/BuscarPersonaServlet.java
+++ src/java/controller/BuscarPersonaServlet.java
@@ -6,30 +6,35 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Persona;
 
 @WebServlet(name = "BuscarPersonaServlet", urlPatterns = {"/buscarPersona.do"})
 public class BuscarPersonaServlet extends HttpServlet {
 
    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        
-    }
-
     // Procesar peticiones GET
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("index.html").forward(request, response);
     }
-
 
     // Procesar peticiones POST
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String dni = request.getParameter("DNI");
+        
+        Persona p = null;
+
+        if(dni.equals("11-1")){
+            p = new Persona("11-1", "Erick Cabrera");
+        }else if(dni.equals("22-2")){
+            p = new Persona("22-2", "Ángeles Ubilla Pérez");
+        }
+        
+        request.setAttribute("persona", p); 
+        request.getRequestDispatcher("resultado.jsp").forward(request, response);
     }
 
 }
--- web/resultado.jsp
+++ web/resultado.jsp
@@ -1,3 +1,4 @@
+<%@page import="model.Persona"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -6,6 +7,19 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Concurso</h1>
+
+        <%
+            Persona p = (Persona) request.getAttribute("persona");
+
+            if(p == null){
+                out.print("Usted no ha ganado! :C"); // echo
+            }else{
+                out.print("Ha ganado!! Felicitaciones "+p.getNombre());
+            }
+        %>
+
+        <!-- Ha ganado!!! <h4>Felicitaciones ${persona.nombre}</h4> -->
+        <a href="index.html">Volver</a>
     </body>
 </html>
